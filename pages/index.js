0af3b531b92dce398fd4558651aae2a48091a0c2
Yuvan Arvind <yuvanarvind@Yuvans-MacBook-Pro.local>
2023-04-23 17:52:37 +0530

    rizz ai UI setup

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,43 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+const [userInput, setUserInput] = useState('');
+const onUserChangedText = (event) => {
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+};
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Rizz AI</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Cool one-liners to strike conversations on dating apps!</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>enter your interests seperated by a comma as shown below</h2>
+          </div>
+          <div className="prompt-container">
+            <textarea
+              placeholder="eg. painting, football, hiking"
+              className="prompt-box"
+              value={userInput}
+              onChange={onUserChangedText}
+            />
+            
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={null}>
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
       </div>
     </div>
   );
